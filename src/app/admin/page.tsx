f011981e48921a1ebb4fa631317b6545b6968d51
Seng Chanthea <sengchantheaz123@gmail.com>
2025-07-13 11:35:24 +0700

    push for new table control user

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,6 @@
 export default function AdminDashboard() {
   return (
-    <div className="text-gray-900 dark:text-white">
+    <div className="text-gray-900">
       <h1 className="text-3xl font-bold mb-6">
         Welcome to the Admin Dashboard
       </h1>
--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -97,16 +97,6 @@ export default function UsersPage() {
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th scope="col" className="p-4">
-              <div className="flex items-center">
-                <input
-                  id="checkbox-all-search"
-                  type="checkbox"
-                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                />
-                <label htmlFor="checkbox-all-search" className="sr-only">checkbox</label>
-              </div>
-            </th>
             <th scope="col" className="px-6 py-3">Name</th>
             <th scope="col" className="px-6 py-3">Position</th>
             <th scope="col" className="px-6 py-3">Status</th>
@@ -116,8 +106,6 @@ export default function UsersPage() {
         <tbody>
           {/* Add your user rows here */}
           <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-            <td className="w-4 p-4">
-            </td>
             <td className="px-6 py-4 font-medium text-gray-900 dark:text-white">
               Neil Sims
               <div className="text-sm text-gray-500">neil.sims@flowbite.com</div>
