c595ef4a1b35cef3eb5b2042c5c6a7385d24335d
Seng Chanthea <sengchantheaz123@gmail.com>
2025-07-13 10:36:51 +0700

    push new idea

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -110,15 +110,9 @@ export default function Navbar() {
               <>
                 <a
                   href="#"
-                  className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-                >
-                  Login
-                </a>
-                <a
-                  href="#"
                   className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                 >
-                  Sign up
+                  Register
                 </a>
               </>
             )}
